85de277068159f46a1334df592b6c9c749705071
AnkushBansal-313 <bansalaman901@gmail.com>
2025-08-28 13:12:13 +0530

    [Ankush] Added NumberFormatDemo.java in String/Level1

---
+++ String/Level1/NumberFormatDemo.java
@@ -0,0 +1,26 @@
+package level1;
+import java.util.Scanner;
+
+public class NumberFormatDemo {
+    static void generateException(String str) {
+        int num = Integer.parseInt(str);
+        System.out.println(num);
+    }
+
+    static void handleException(String str) {
+        try {
+            int num = Integer.parseInt(str);
+            System.out.println(num);
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException handled");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        try { generateException(str); } catch (Exception e) { System.out.println("Exception occurred"); }
+        handleException(str);
+     
+    }
+}
