a685ffa1158095f7b0618df48451c9e4a5cc9dbb
AnkushBansal-313 <bansalaman901@gmail.com>
2025-08-28 13:12:00 +0530

    [Ankush] Added NullPointerDemo.java in String/Level1

---
+++ String/Level1/NullPointerDemo.java
@@ -0,0 +1,20 @@
+public class NullPointerDemo {
+    static void generateNullPointer() {
+        String text = null;
+        System.out.println(text.length());
+    }
+
+    static void handleNullPointer() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException handled");
+        }
+    }
+
+    public static void main(String[] args) {
+        try { generateNullPointer(); } catch (Exception e) { System.out.println("Exception occurred"); }
+        handleNullPointer();
+    }
+}
