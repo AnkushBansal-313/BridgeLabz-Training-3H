dd42b96f2b66ffbe27bd0589a4993b549e3fe9d7
AnkushBansal-313 <bansalaman901@gmail.com>
2025-08-28 13:15:05 +0530

    [Ankush] Added StringLengthWithoutBuiltIn.java in String/Level2

---
+++ String/Level2/StringLengthWithoutBuiltIn.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class StringLengthWithoutBuiltIn {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.next();
+        
+        int customLength = findLength(input);
+        int builtInLength = input.length();
+        
+        System.out.println("Custom length: " + customLength);
+        System.out.println("Built-in length: " + builtInLength);
+    }
+    
+    public static int findLength(String str) {
+        int count = 0;
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            return count;
+        }
+    }
+}
