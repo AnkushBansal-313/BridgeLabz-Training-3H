fbba52d20c67dce52661ea975154363fde076242
AnkushBansal-313 <bansalaman901@gmail.com>
2025-09-03 14:43:06 +0530

    Recovered Basic-opps folder

---
+++ Basic-opps/Level 2/Student/StudentMain.java
@@ -0,0 +1,10 @@
+package Basic-opps.Level 2.Student;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student s1 = new Student("Ankush", 101, 92.5);
+        s1.calculateGrade();
+        s1.displayDetails();
+        
+    }
+}
